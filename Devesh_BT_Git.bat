ffaccaab54677f8a1d824fb32dd78f3e0100d5b9
deveshpingle <devesh.pingle1234@gmail.com>
2025-01-19 17:33:23 +0530

    initial commit

---
+++ Devesh_BT_Git.bat
@@ -0,0 +1,12 @@
+git config --global --unset user.name
+git config --global --unset user.email
+git config --global --unset credential.helper
+git config --global user.name "deveshpingle"
+git config --global user.email "devesh.pingle1234@gmail.com" 
+git init
+git add .
+git commit -m "initial commit"
+git remote add origin https://github.com/deveshpingle/Portfolio.git
+git fetch --all
+git checkout -b "dev/Portfolio_2"
+git push origin dev/Portfolio_2
