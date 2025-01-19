08be8988851b2c7fffde0706bcbc50f0d926e51a
deveshpingle <100547114+deveshpingle@users.noreply.github.com>
2025-01-19 18:21:44 +0530

    Update Portfolio.jsx

--- animated-portfolio-completed/src/components/portfolio/Portfolio.jsx
+++ animated-portfolio-completed/src/components/portfolio/Portfolio.jsx
@@ -5,27 +5,27 @@ import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 const items = [
   {
     id: 1,
-    title: "React Commerce",
+    title: "MapTask(Task Management)",
     img: "https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    desc: "A MapTask is a software tool designed to help individuals and teams organize, prioritize, and manage their tasks effectively. It streamlines productivity by providing a centralized platform to track and complete work.",
   },
   {
     id: 2,
-    title: "Next.js Blog",
+    title: "Biller(Invoice Generator)",
     img: "https://images.pexels.com/photos/18023772/pexels-photo-18023772/free-photo-of-close-up-of-a-person-holding-a-wristwatch.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    desc: "An Invoice Generator Application is a software tool designed to simplify the process of creating, managing, and sending invoices for businesses or freelancers. It automates invoicing workflows, reduces manual effort, and ensures professional and accurate billing",
   },
   {
     id: 3,
-    title: "Vanilla JS App",
+    title: "CRM",
     img: "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    desc: "A Customer Relationship Management (CRM) application is a software tool designed to help businesses manage their interactions with customers, clients, and prospects. It centralizes customer information, automates sales processes, improves customer service, and helps businesses build strong relationships.",
   },
   {
     id: 4,
-    title: "Music App",
+    title: "Portfolio",
     img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    desc: "A Portfolio Application is a software platform that allows individuals, freelancers, creatives, or businesses to showcase their work, achievements, projects, and skills in a visually appealing and organized way. It serves as a digital representation of a person's or organization's professional journey, making it easier to attract clients, collaborators, or job opportunities",
   },
 ];
 
